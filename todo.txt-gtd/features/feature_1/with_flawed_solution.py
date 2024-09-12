862e8bc304dcfe0cc492e97ed8d9322baa397d54
akshatjain-beam <akshat.j1@turing.com>
2024-09-12 11:09:36 +0530

    indentation fixed
    to match the original code indentation

--- todo.txt-gtd/features/feature_1/with_flawed_solution.py
+++ todo.txt-gtd/features/feature_1/with_flawed_solution.py
@@ -1,21 +1,21 @@
-def LineGen(self, todotxt):
-    curr_proj = None
-    deferredTasks = []
+    def LineGen(self, todotxt):
+        curr_proj = None
+        deferredTasks = []
 
-    for line in todotxt.splitlines():
-        if HeaderProj(line, "" if curr_proj is None else self[curr_proj].tasks[-1].text):
-            curr_proj = HeaderProj(line, "" if curr_proj is None else self[curr_proj].tasks[-1].text)
+        for line in todotxt.splitlines():
+            if HeaderProj(line, "" if curr_proj is None else self[curr_proj].tasks[-1].text):
+                curr_proj = HeaderProj(line, "" if curr_proj is None else self[curr_proj].tasks[-1].text)
 
-        if TaskProj(line):
-            if TaskProj(line) != curr_proj:
-                deferredTasks.append(line)
+            if TaskProj(line):
+                if TaskProj(line) != curr_proj:
+                    deferredTasks.append(line)
+                else:
+                    yield curr_proj, line
             else:
-                yield curr_proj, line
-        else:
-            if curr_proj:
-                yield curr_proj, line
-            else:
-                deferredTasks.append(line)
+                if curr_proj:
+                    yield curr_proj, line
+                else:
+                    deferredTasks.append(line)
 
-    for line in deferredTasks:
-        yield TaskProj(line), line
+        for line in deferredTasks:
+            yield TaskProj(line), line
