22d1edb59f794c35064e46de47ede405e8af942e
akshatjain-beam <akshat.j1@turing.com>
2024-09-13 08:38:48 +0530

    updated flawed solution

--- socials/features/feature_2/with_flawed_solution.py
+++ socials/features/feature_2/with_flawed_solution.py
@@ -1,2 +1,4 @@
 FACEBOOK_URL_REGEXS = [
-    r'^http(s)?://(www\.)?facebook\.com/[A-Za-
+    r'^http(s)?://(www\.)?facebook\.com/[A-Za-z0-9_\.\-]+/?$',
+    r'^http(s)?://(www\.)?facebook\.com/profile\.php\?id=[0-9]+/?$'
+]
