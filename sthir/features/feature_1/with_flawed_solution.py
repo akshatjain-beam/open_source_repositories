e5f34e4a87b99ef7111784efbb25637e5c722e11
akshatjain-beam <akshat.j1@turing.com>
2024-09-18 09:20:20 +0530

    updated prompt and features
    feature 1

--- sthir/features/feature_1/with_flawed_solution.py
+++ sthir/features/feature_1/with_flawed_solution.py
@@ -1,4 +1,6 @@
 ```
+def process_documents(documents):
+    base2p15_arrs = list()
     for document in documents:
         bit_arr = bitarray()
         with open(document[0], "rb") as f:
@@ -6,4 +8,5 @@
         base2p15 = base2p15_encode(bit_arr.to01())
         document = [base2p15] + document[1:]
         base2p15_arrs.append(document)
+    return base2p15_arrs
 ```
--- sthir/features/feature_1/with_golden_solution.py
+++ sthir/features/feature_1/with_golden_solution.py
@@ -1,10 +1,15 @@
 ```
+def process_documents(documents):
+    base2p15_arrs = list()
     for document in documents:
         bit_arr = bitarray()
         with open(document[0], "rb") as f:
             bit_arr.fromfile(f)
         base2p15_arrs.append([base2p15_encode(bit_arr.to01()), 
-                                document[1], 
-                                document[2], 
-                                document[3], document[0]])
+                              document[1], 
+                              document[2], 
+                              document[3], 
+                              document[0]])
+    
+    return base2p15_arrs
 ```
