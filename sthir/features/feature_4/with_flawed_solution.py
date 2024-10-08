9401547b6d54ca5d85f4d92d483b499bb3cbc91d
akshatjain-beam <akshat.j1@turing.com>
2024-10-08 16:25:10 +0530

    updated prompt and features

--- sthir/features/feature_4/with_flawed_solution.py
+++ sthir/features/feature_4/with_flawed_solution.py
@@ -1,8 +1,12 @@
 ```
+    padding_length = int(base2p15[0], 16)
     bit_string = ""
     offset = 0xa1
-    padding_bits = int(base2p15[0], 16)
-    for character in base2p15[1:]:
-        bit_string += f'{ord(character) - offset:015b}'
-    return bit_string[:-padding_bits]
+
+    for char in base2p15[1:]:
+        bit_string += bin(ord(char) - offset)[2:].zfill(15)
+
+    # Remove padding bits from the last 15-bit sequence
+    bit_string = bit_string[:-padding_length]
+    return bit_string
 ```
