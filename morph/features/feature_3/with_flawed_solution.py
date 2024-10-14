9b41c21d661e68af28d0acd24e988b32397fd6e6
akshatjain-beam <akshat.j1@turing.com>
2024-10-14 16:09:50 +0530

    status = LLM Not able to solve
    feature 3

---
+++ morph/features/feature_3/with_flawed_solution.py
@@ -0,0 +1,10 @@
+```
+if items is not None:
+    source = {k[len(prefix):]: v
+                for k, v in items
+                if k.startswith(prefix)}
+else:
+    source = {attr[len(prefix):]: getattr(source, attr)
+                for attr in properties(source)
+                if attr.startswith(prefix)}
+```
