8c33369cc3d6ae28ccd728e3175559021b0762e7
akshatjain-beam <akshat.j1@turing.com>
2024-10-18 10:12:24 +0530

    bootleneck feature 5
    updated-
    1. flawed solution

--- bottle-neck/features/feature_5/with_flawed_solution.py
+++ bottle-neck/features/feature_5/with_flawed_solution.py
@@ -1,8 +1,8 @@
 ```
-		status_line = status_line.lower().replace(' ', '_')
-		status_line = ''.join(
-		    ch for ch in status_line if ch.isalnum() or ch == '_'
-		)
-		method = getattr(cls, status_line, cls.not_implemented)
-		return method(errors=msg)
+        status_line = status_line.lower().replace(' ', '_')
+        status_line = ''.join(
+            ch for ch in status_line if ch.isalnum() or ch == '_'
+        )
+        method = getattr(cls, status_line, cls.not_implemented)
+        return method(errors=msg)
 ```
