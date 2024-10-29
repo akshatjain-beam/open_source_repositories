f62a061ab4ca32669dc233a71fd9cc7440bf9e3a
akshatjain-beam <akshat.j1@turing.com>
2024-10-29 12:31:17 +0530

    Updated feature
    Updates-
    1. Prompt
    2. Flawed solution

--- arabic_pronunciation/features/feature_4/with_flawed_solution.py
+++ arabic_pronunciation/features/feature_4/with_flawed_solution.py
@@ -2,13 +2,13 @@
         if isinstance(fixedWords[wordConsonants], list):
             done = False
             for pronunciation in fixedWords[wordConsonants]:
-                pronunciationPhonemes = pronunciation.split(u' ')
-                if len(pronunciationPhonemes) > 0:
-                    lastPhoneme = pronunciationPhonemes[-1]
-                    if lastPhoneme in lastLetter:
+                for lastLetterOption in lastLetter:
+                    if pronunciation.endswith(lastLetterOption):
                         pronunciations.append(pronunciation)
                         done = True
                         break
+                if done:
+                    break
             if not done:
                 pronunciations.append(fixedWords[wordConsonants][0])
         else:
