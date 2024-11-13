e058d12baf94e39f92aa01c56ac96fb964facb0b
akshatjain-beam <akshat.j1@turing.com>
2024-11-13 18:24:30 +0530

    updated feature

--- rtc/features/feature_6/with_hole.go
+++ rtc/features/feature_6/with_hole.go
@@ -29,22 +29,20 @@ func (vErr ValidationError) Error() string {
 
 // Create a `fullVErrRegex` which is the regular expression, which matches strings with validation error: , followed by any number of characters (which are captured in groups), and ending with At.
 //Create a `tinyVErrRegex` which is the regular expression, which matches strings with validation error: , followed by any number of characters (which are captured in groups)
+//
+// Than Create `UnwrapValidationError` function that takes an error, checks if it's a validation error, 
+// and extracts a clean message from it. If the error is nil or 
+// contains no message, it returns nil. If the error string contains 
+// the substring ". At", it uses a regex pattern tailored for full validation errors; 
+// otherwise, it applies a regex pattern for smaller errors. The function trims 
+// any leading "validation error: " and trailing ". At" from the extracted message 
+// before returning it as a new error.
+  
+// Parameters:
+//   vErr (error): The error to be unwrapped. If it's nil or its message is empty, 
+//   nil is returned.
+
+// Returns:
+//   error: A new error with the extracted validation message, or nil if the input 
+//   error is empty or nil.
 $PlaceHolder$
-
-
-// UnwrapValidationError will undo .Error() boilerplate (skipping origin information, and giving .Err)
-func UnwrapValidationError(vErr error) error {
-	if vErr == nil || vErr.Error() == "" {
-		return nil
-	}
-	strErr := vErr.Error()
-	var vErrRegex *regexp.Regexp
-	if strings.Contains(strErr, ". At") {
-		vErrRegex = fullVErrRegex
-	} else {
-		vErrRegex = tinyVErrRegex
-	}
-	strErr = vErrRegex.FindAllString(strErr, 1)[0]
-	strErr = strings.TrimSuffix(strings.TrimPrefix(strErr, "validation error: "), ". At")
-	return errors.New(strErr)
-}
