f5ecbd8b3f6484ce3a114f25356d787f11119924
akshatjain-beam <akshat.j1@turing.com>
2024-11-13 09:11:54 +0530

    Updated feature

--- rtc/internal/name/name_test.go
+++ rtc/internal/name/name_test.go
@@ -2,36 +2,7 @@ package name
 
 import "testing"
 
-func Test_toSnakeCase(t *testing.T) {
-	cases := [][]string{
-		{"testCase", "test_case"},
-		{"TestCase", "test_case"},
-		{"Test Case", "test_case"},
-		{" Test Case", "test_case"},
-		{"Test Case ", "test_case"},
-		{" Test Case ", "test_case"},
-		{"test", "test"},
-		{"test_case", "test_case"},
-		{"Test", "test"},
-		{"", ""},
-		{"ManyManyWords", "many_many_words"},
-		{"manyManyWords", "many_many_words"},
-		{"AnyKind of_string", "any_kind_of_string"},
-		{"numbers2and55with000", "numbers_2_and_55_with_000"},
-		{"JSONData", "json_data"},
-		{"userID", "user_id"},
-		{"userIDs", "user_i_ds"}, // see 5e49e3d
-		{"AAAbbb", "aa_abbb"},
-	}
-	for _, i := range cases {
-		in := i[0]
-		out := i[1]
-		result := ToSnakeCase(in)
-		if result != out {
-			t.Error("'" + in + "'('" + result + "' != '" + out + "')")
-		}
-	}
-}
+
 
 func TestToCamel(t *testing.T) {
 	cases := [][]string{
