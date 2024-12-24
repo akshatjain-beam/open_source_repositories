14ccaa223535b386d60235a00b11d3e525a2cd51
Akshat Jain <113326157+akshatjain-beam@users.noreply.github.com>
2024-12-24 17:13:48 +0530

    repo: Lorey/socials (#2)
    * status - LLM Not able to SOLVE
    
    * fixed to add correct folders to ignore
    
    * updated with removed warning
    
    * status = LLM not able to solve
    
    updated prompt and flawed response for both features
    
    * updated flawed solution
    
    * Squashed commit of the following:
    
    commit 7b23595ce84d3d7916db5d22ff3bdd97841a90b8
    Author: akshatjain-beam <akshat.j1@turing.com>
    Date:   Tue Sep 24 10:25:56 2024 +0530
    
        Update .gitignore
    
    commit 080cb58ae1b95b6d161b7cef14779d4b44689126
    Author: akshatjain-beam <akshat.j1@turing.com>
    Date:   Tue Sep 24 08:35:23 2024 +0530
    
        Update feature_folder_creator.py
    
    commit 92c5a7fb4bc933f92374360ccda11db2c2e66f8e
    Author: akshatjain-beam <akshat.j1@turing.com>
    Date:   Mon Sep 23 13:29:52 2024 +0530
    
        add folder automation script for `features`
    
    * updated prompt and features
    
    feature 1
    
    * updated features and prompt
    
    feature 1
    
    * status = LLM SOLVED
    
    updated features
    
    * Update test_socials.py
    
    ---------
    
    Co-authored-by: akshatjain-beam <akshat.j1@turing.com>

---
+++ socials/features/feature_1/with_golden_solution.py
@@ -0,0 +1,7 @@
+LINKEDIN_URL_REGEXS = [
+    # private
+    r'^http(s)?://([\w]+\.)?linkedin\.com/in/[A-Za-z0-9_-]+/?$',
+    r'^http(s)?://([\w]+\.)?linkedin\.com/pub/[A-Za-z0-9_-]+(\/[A-z 0-9]+){3}/?$',
+    # companies
+    r'^http(s)?://(www\.)?linkedin\.com/company/[A-Za-z0-9_-]+/?$',
+]
---
+++ socials/features/feature_2/with_flawed_solution.py
@@ -0,0 +1,6 @@
+FACEBOOK_URL_REGEXS = [
+    r'^http(s)?://(www\.)?facebook\.com/[A-Za-z0-9_.-]+/?$',
+    r'^http(s)?://(www\.)?facebook\.com/profile\.php\?id=[0-9]+/?$',
+    r'^http(s)?://(fb|www\.fb)\.com/[A-Za-z0-9_.-]+/?$',
+    r'^http(s)?://(fb|www\.fb)\.com/profile\.php\?id=[0-9]+/?$',
+]
