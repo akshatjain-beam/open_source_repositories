386331d1d835566cc2d654177d92c169f2eccd6b
Akshat Jain <113326157+akshatjain-beam@users.noreply.github.com>
2024-12-24 17:19:08 +0530

    repo: davesteele/todo.txt-gtd [feature 1 & base] (#7)
    * status = LLM not able to SOLVE
    
    feature 1
    
    * indentation fixed
    
    to match the original code indentation
    
    * indentation fixed
    
    * Create flawed_solution_failure_reason.txt
    
    feature 1
    
    * Update test_cleanup.py
    
    feature 1
    
    * repo: davesteele/todo.txt-gtd [feature 2] (#6)
    
    * status = LLM not able to SOLVE
    
    feature 2
    
    * indentation fixed
    
    * updated complete feature 2
    
    updated tests
    
    * status = LLM SOLVED
    
    ---------
    
    Co-authored-by: akshatjain-beam <akshat.j1@turing.com>
    
    ---------
    
    Co-authored-by: akshatjain-beam <akshat.j1@turing.com>

---
+++ todo.txt-gtd/features/feature_1/with_golden_solution.py
@@ -0,0 +1,29 @@
+```
+    def LineGen(self, todotxt):
+        current_project = None
+
+        deferredTasks = []
+        prev_line = ""
+
+        for line in todotxt.split("\n"):
+
+            if HeaderProj(line, prev_line):
+                current_project = HeaderProj(line, prev_line)
+
+            taskProj = TaskProj(line)
+            if taskProj:
+                if current_project and (current_project != taskProj):
+                    deferredTasks.append(line)
+                else:
+                    yield (taskProj, line)
+            else:
+                if current_project:
+                    yield (current_project, line)
+                else:
+                    deferredTasks.append(line)
+
+            prev_line = line
+
+        for line in deferredTasks:
+            yield (TaskProj(line), line)
+```
