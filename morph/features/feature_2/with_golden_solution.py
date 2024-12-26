c0761d5148141896f19d702e07c30b08745acb0c
Akshat Jain <113326157+akshatjain-beam@users.noreply.github.com>
2024-12-26 16:56:42 +0530

    repo: metagriffin/morph [feature 2]  (#67)
    * status = LLM Not able to solve
    
    feature 2
    
    * fixed the feature
    
    * Fixed the feature_2
    
    Fixed and updated golden solution and flawed solution
    
    ---------
    
    Co-authored-by: akshatjain-beam <akshat.j1@turing.com>

---
+++ morph/features/feature_2/with_golden_solution.py
@@ -0,0 +1,14 @@
+```
+def isdict(obj):
+  '''
+  Returns True if `obj` is a dict-like object (but not a string or
+  list); i.e. a dict, subclass thereof, or having an interface that
+  supports key, value, and item iteration.
+  '''
+  return \
+    not isstr(obj) \
+    and ( isinstance(obj, dict) \
+          or ( callable(getattr(obj, 'keys', None)) \
+               and callable(getattr(obj, 'values', None)) \
+               and callable(getattr(obj, 'items', None)) ))
+```
